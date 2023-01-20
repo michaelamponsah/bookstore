4a7dbbde5c4ceb555b20c0e73cd15da10cf6fe84
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-20 10:22:29 +0000

    Add prop-types pkg to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Header from './components/header/Header';
+import Book from './components/book/Book';
+// import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Book />
     </div>
   );
 }
