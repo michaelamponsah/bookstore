f85402df6f0dce8aaea273f79b30232616fbdaa8
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-24 16:00:46 +0000

    Add functionality to add and remove book object from list

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { useDispatch } from 'react-redux';
 import Card from '../card/Card';
 import styles from './Book.module.css';
+import { removeBook } from '../../redux/books/books';
 
 const Book = ({ book }) => {
   const {
     id, genre, title, author,
   } = book;
+
+  const dispatch = useDispatch();
+
+  const handleDelete = (bookData) => {
+    dispatch(removeBook(bookData));
+  };
 
   return (
     <Card id={id}>
@@ -18,7 +26,7 @@ const Book = ({ book }) => {
             <p className={`${styles['author-name']}`}>{author}</p>
           </div>
           <div className={styles.comments}>
-            <button type="button">Remove</button>
+            <button type="button" onClick={() => handleDelete(book)}>Remove</button>
           </div>
         </div>
         <div>Stats 80%</div>
