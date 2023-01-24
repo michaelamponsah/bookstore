0f313042238e611b64e308bf51f48c975949de3b
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-24 16:01:09 +0000

    src/pages/categories/CategoriesPage.jsx

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,7 +1,7 @@
 // Actions
 const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
 
-const categoriesReducer = (state = [], action) => {
+const categoriesReducer = (state='', action) => {
   switch (action.type) {
     case CHECK_STATUS:
       return 'Under Construction';
