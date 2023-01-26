280fd3eb158ea68d0c4a39243600edc21a15bdf4
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-26 13:41:04 +0000

    Style categories page

--- src/pages/categories/CategoriesPage.jsx
+++ src/pages/categories/CategoriesPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../../redux/categories/categories';
+import styles from './CategoriesPage.module.css';
 
 const CategoriesPage = () => {
   const dispatch = useDispatch();
@@ -11,9 +12,9 @@ const CategoriesPage = () => {
   };
   return (
     <div className="container">
-      <p>{statusData}</p>
+      <p className={styles.message}>{statusData}</p>
       <br />
-      <button type="button" onClick={handleCheckStatus}>Check Status</button>
+      <button type="button" className="btn-filled" onClick={handleCheckStatus}>Check Status</button>
     </div>
   );
 };
